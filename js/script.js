c69cfc98c29aae6833bd3e9367f22fe089b70fe8
Kyle <kzunino@gmail.com>
2019-04-02 13:12:07 -0700

    refactoring woo

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ FSJS project 2 - List Filter and Pagination
 
 //global variables
 
-const pageDiv = document.querySelector('.page'); // stores the div with class value page into a variable
-const ul = document.querySelector('.student-list'); //stores ul with student-list class
+const pageDiv = document.querySelector('.page');             // stores the div with class value page into a variable
+const ul = document.querySelector('.student-list');         //stores ul with student-list class
 const pageHeader = document.querySelector('.page-header'); //selects page header div *Note: leave off whitespace and cf from class tag.
 let studentList = ul.querySelectorAll('li.student-item');
-const pages = Math.ceil(studentList.length/10); // divides index by page numbers and rounds up to fit values on the last page.
+const pages = Math.ceil(studentList.length/10);           // divides index by page numbers and rounds up to fit values on the last page.
 let resultList = [];
-let searchCount = '';                                  //emptry searchCount Variable
+let searchCount = '';                                    //emptry searchCount Variable
 
 
 
@@ -41,10 +41,10 @@ searchDiv.appendChild(button);
 
 
 const showPage = (studentList, page) => {
-  let lastListItem = (page * 10) - 1;         //stores the last item's index value
-  let firstListItem = (lastListItem - 9);     //stores the first item's index value
-  for (var i = 0; i < studentList.length ; i += 1) {    //itterates through the student list
-    if (i >= firstListItem && i <= lastListItem){   //conditional statement to iterate through list segments (note to self don't use studentList[i]) and hide or display list items
+  let lastListItem = (page * 10) - 1;                    //stores the last item's index value
+  let firstListItem = (lastListItem - 9);               //stores the first item's index value
+  for (var i = 0; i < studentList.length ; i += 1) {   //itterates through the student list
+    if (i >= firstListItem && i <= lastListItem){      //conditional statement to iterate through list segments (note to self don't use studentList[i]) and hide or display list items
       studentList[i].style.display = '';
     }else{
       studentList[i].style.display = 'none'; //hides list items
@@ -60,42 +60,31 @@ const appendPageLinks = (pages, studentList) => {
   };
 
   const createElement = (elementName, setClass, domNode) => { //refactored code for DRY
-    const element = document.createElement(elementName);
-    element.className += setClass;
-    domNode.appendChild(element);
+    const element = document.createElement(elementName);      //creates new DOM elements
+    element.className += setClass;                            //gives dom elements new class names
+    domNode.appendChild(element);                             // appends new elemement to DOM
     return element;
   };
 
-  const newDiv = createElement('div', 'pagination', pageDiv);  //creates newDiv, class, and appends it.
-  const newUl = createElement('ul', 'pages', newDiv); // creates newUL, class, and appends it.
-
-
-  // const newDiv = document.createElement('div');  //creates new div
-  // pageDiv.appendChild(newDiv);  //appends paginationDiv to div.page
-  // newDiv.className += 'pagination';  // assigns class name pagination to div
-  // const newUl = document.createElement('ul'); // creates ul for page links
-  // newUl.className = 'pages';
-  // newDiv.appendChild(newUl); // appends the page links to paginationDiv
+  const newDiv = createElement('div', 'pagination', pageDiv);  //calls function to create newDiv, class, and appends it.
+  const newUl = createElement('ul', 'pages', newDiv);
 
   const selectNewUl = document.querySelector('.pages')
 
       for (var i = 1; i <= pages; i+= 1){
-        const newLi = document.createElement('li');  //creates a list item in pageLinks
-        newUl.appendChild(newLi);  //appends li to pagelinks
-        const aTag = document.createElement('a');
-        newLi.appendChild(aTag);
-        aTag.setAttribute('href','#'); //sets attribute for href link
-        aTag.textContent = i;   //changes page numbers per each itteration
+        const newLi = createElement('li', undefined, newUl);   //creates a list item in pageLinks and appends to newUL. (undefined skips middle parameter source:https://stackoverflow.com/questions/8356227/skipping-optional-function-parameters-in-javascript)
+        const aTag = createElement('a', undefined, newLi);    // creates a tags and appends to newLI
+        aTag.setAttribute('href','#');                        //sets attribute for href link
+        aTag.textContent = i;                                 //changes page numbers per each itteration
       }
   const aTagList = document.querySelectorAll('a');
-  // aTagList[0].className = 'active';                 //adds active class to first as default
   selectNewUl.addEventListener('click', (e) => {    //uses bubbling to target ancestor ul of a tags.
     if (event.target.tagName === 'A'){
       showPage(studentList, event.target.textContent); //calls function with aTag's text content, which is a corresponding page number;
       for (var i = 0; i < aTagList.length; i += 1){
-        aTagList[i].classList.remove('active');
+        aTagList[i].classList.remove('active');       //removes active class from item
       }
-    event.target.className = 'active';
+    event.target.className = 'active';                //activates CSS for clicked page link
       }
     });
     if (document.contains(document.querySelector('a'))) { // if statement checks to see if a tag exists.
